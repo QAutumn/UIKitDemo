2806158cb32b4e2cb80f36658b81797750bad281
cheshui <584682341@qq.com>
2021-12-26 18:04:34 +0800

    navigatoinBar

--- capcut_demo/AppDelegate.swift
+++ capcut_demo/AppDelegate.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
-
+/*
+ 不习惯使用SceneDelegate可以将其删除，按原来的方式进行项目开发
+ 删除操作：
+ 1、SceneDelegate文件删除
+ 2、AppDelegate文件中函数application(_:configurationForConnecting:options:)和application(_:didDiscardSceneSessions:)删除
+ 3、Info.plist文件中Application Scene Manifest删除
+ */
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let nav = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = nav
+        window?.backgroundColor = .purple
+        window?.makeKeyAndVisible()
         return true
     }
-
 }
 
--- capcut_demo/ViewController.swift
+++ capcut_demo/ViewController.swift
@@ -9,12 +9,29 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    lazy var btn = makeUIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        print("helloworld !")
-        print("xixi")
-        print("!2#")
+        setupUI()
+    }
+    
+    func setupUI() {
+        view.addSubview(btn)
+        btn.snp.makeConstraints { make in
+            make.center.width.equalToSuperview()
+            make.height.equalTo(40)
+        }
+    }
+    func makeUIButton() -> UIButton {
+        let btn = UIButton()
+        btn.backgroundColor = .blue
+        btn.setTitle("go", for: .normal)
+        btn.addTarget(self, action: #selector(go), for: .touchUpInside)
+        return btn
+    }
+    
+    @objc func go() {
+        navigationController?.pushViewController(secondVC(), animated: true)
     }
 }
 
